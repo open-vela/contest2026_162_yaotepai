51a807e15b6ff5c48d572906814f5ac079a5007f
Yang Hongbo <yanghongbo@iotpi.io>
2026-09-16 16:26:46 +0800

    fixed MPI4 Nand init

--- board/sf32lb58/sf32lb58-nxboot/src/sf32lb58_boot_bringup.c
+++ board/sf32lb58/sf32lb58-nxboot/src/sf32lb58_boot_bringup.c
@@ -25,6 +25,8 @@
 #  include "sf32lb_nand.h"
 #endif
 
+#include "bf0_hal_rcc.h"
+#include "sfconfig.h"
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
@@ -43,9 +45,37 @@
 #  define SF32LB58_NAND_PART2_SIZE    0x00800000  /* 8 MB */
 #endif
 
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
+static UART_HandleTypeDef uart_handle;
+
+static void boot_uart_init(void)
+{
+    memset(&uart_handle, 0, sizeof(UART_HandleTypeDef));
+
+#ifdef BSP_USING_UART4
+    HAL_PIN_Set(PAD_PB37, USART4_TXD, PIN_PULLUP, 0);
+    HAL_PIN_Set(PAD_PB36, USART4_RXD, PIN_PULLUP, 0);
+    uart_handle.Instance = USART4;
+    HAL_RCC_LCPU_ClockSelect(RCC_CLK_MOD_LP_PERI, RCC_CLK_PERI_HXT48);
+#elif defined(BSP_USING_UART1)
+    HAL_PIN_Set(PAD_PA31, USART1_TXD, PIN_PULLUP, 1);
+    HAL_PIN_Set(PAD_PA32, USART1_RXD, PIN_PULLUP, 1);
+    uart_handle.Instance = USART1;
+    HAL_RCC_HCPU_ClockSelect(RCC_CLK_MOD_HP_PERI, RCC_CLK_PERI_HXT48);
+#else
+#error Select UART1/4 for bootloader
+#endif
+
+    uart_handle.Init.BaudRate   = 1000000;
+    uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+    uart_handle.Init.StopBits   = UART_STOPBITS_1;
+    uart_handle.Init.Parity     = UART_PARITY_NONE;
+    uart_handle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+    uart_handle.Init.Mode       = UART_MODE_TX_RX;
+    uart_handle.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&uart_handle) != HAL_OK)
+    {
+    }
+}
 
 /****************************************************************************
  * Name: sf32lb58_boot_bringup
@@ -55,7 +85,6 @@
  *   Initializes NAND flash and registers partitions for OTA.
  *
  ****************************************************************************/
-
 int sf32lb58_boot_bringup(void)
 {
   int ret = OK;
